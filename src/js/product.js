ce928ca946afda343f0ed6def1cdd9c06ed47904
gerson <gersonmoliver@gmail.com>
2025-05-31 15:17:47 -0700

    fixed bug when open the product detail

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,9 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productID = getParam("product");
+let productID = getParam("products");
+
+
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
